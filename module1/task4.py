0fc5c7b0ff11ae7c364187e2fffbee935ecbaaf2
Daniil Yefimov <danyefimoff@gmail.com>
2020-11-06 14:21:27 +0300

    first module

---
+++ module1/task4.py
@@ -0,0 +1,25 @@
+
+# Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
+# Точность указывается в функции round(a, b)
+# где a, это число которое надо округлить, а b количество знаков после запятой
+# более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
+
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+
+
+# TODO Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+#   Три песни звучат ХХХ.XX минут
+
+
+# TODO Распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
+#   А другие три песни звучат приблизительно ХХХ минут
